06a0eced358e1df76f9db3b8c9cc822962e425b9
krzysiek <krzys@piekarczyk.eu>
2019-04-16 09:25:40 +0200

    make com.kpiekarczyk.lox package, delete Main class

---
+++ src/com/kpiekarczyk/lox/Lox.java
@@ -0,0 +1,10 @@
+package com.kpiekarczyk.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Lox {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
