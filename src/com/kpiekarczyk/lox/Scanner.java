c7caef05ddee0c5253793f6b89d7bf84f3d2bbe6
krzysiek <krzys@piekarczyk.eu>
2019-04-17 17:58:12 +0200

    declare Scanner class

---
+++ src/com/kpiekarczyk/lox/Scanner.java
@@ -0,0 +1,34 @@
+package com.kpiekarczyk.lox;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.kpiekarczyk.lox.TokenType.*;
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;
+    private int current = 0;
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+}
