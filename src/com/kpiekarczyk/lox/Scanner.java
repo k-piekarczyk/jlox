693f1126f9d3dc8f1428a6fde6c860652de386f8
krzysiek <krzys@piekarczyk.eu>
2019-04-17 18:44:02 +0200

    extending Scanner class

--- src/com/kpiekarczyk/lox/Scanner.java
+++ src/com/kpiekarczyk/lox/Scanner.java
@@ -45,6 +45,14 @@ class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
+            case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+            case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+            case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
+            case '>': addToken(match('=') ? GREATER_EQUAL : GREATER_EQUAL); break;
+
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
         }
     }
 
@@ -60,5 +68,13 @@ class Scanner {
     private void addToken(TokenType type, Object literal) {
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char expected) {
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != expected) return false;
+
+        current ++;
+        return true;
     }
 }
